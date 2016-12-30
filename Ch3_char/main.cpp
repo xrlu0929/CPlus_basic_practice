99956ecca02d9f9195c4ea150e3ec93ae25f889c
Xinrui Lu <annaxlu@xinruis-mbp.nyc.rr.com>
2016-12-30 18:32:02 -0500

    Initial Commit

---
+++ Ch3_char/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ch3_char
+//
+//  Created by Xinrui Lu on 12/30/16.
+//  Copyright © 2016 Xinrui Lu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
